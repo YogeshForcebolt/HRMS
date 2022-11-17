a1289db952c4002b0a6a304ff10cede7aa9ce616
Sandeep <sandeepsolanki20018@gmail.com>
2022-11-17 17:42:25 +0530

    LAyout uploaded

---
+++ HRMS/HRMS/Controllers/LoginController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HRMS.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult LoginPage()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
